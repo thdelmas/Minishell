5bb38ac579d92e973f7f95ca832bf5c52aeb0e29
symas <symas@symas.rip>
2019-06-30 11:35:32 +0200

    MAJ main.c up|loop|down

--- srcs/main.c
+++ srcs/main.c
@@ -11,21 +11,13 @@
 /* ************************************************************************** */
 
 #include "sh.h"
+#include "libft.h"
 
 int		main(int ac, char **av, char **env)
 {
-	char	**av_tmp;
-	t_sh	*sh;
-
-	if (ac == 1)
-	{
-		if (!(av_tmp = sh_tabdup(av)))
-			return (0);
-		if (!(sh = sh_init_sh(env)))
-			return (0);
-		sh_free_tab(&av_tmp);
-		sh_loop(sh);
-		sh_free_sh(&sh);
-	}
+	ft_putendl("Minishell: main.c\n---");
+	//sh_startup();
+	//sh_loop();
+	//sh_shutdown();
 	return (0);
 }
